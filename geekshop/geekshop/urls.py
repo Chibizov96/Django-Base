d6e4a304bc5e138ce9e43627f07f70690dbdd73a
Yakov Chibizov <chibizov96@gmail.com>
2021-06-28 07:22:25 +0300

    Lesson 2 done
    Its HARD XD

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -6,8 +6,8 @@ from geekshop.views import index, contacts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('contact.html', contacts),
-    path('products.html', include('mainapp.urls'))
+    path('', index, name="index"),
+    path('contacts/', contacts, name="contacts"),
+    path('products/', include('mainapp.urls', namespace="products"))
 ]
 
--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
+    title = 'магазин'
+    context = {
+        'title': title
+    }
+    return render(request, 'geekshop/index.html', context=context)
 
 
 def contacts(request):
-    return render(request, "contact.html")
-
-def products(request):
-    return render(request, "products.html")
+    return render(request, 'geekshop/contact.html')
