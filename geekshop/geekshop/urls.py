ebca381dd995dc32371d4da601fb3a7fef6fa21d
Yakov Chibizov <chibizov96@gmail.com>
2021-06-29 22:43:48 +0300

    Lesson 3 done

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -3,6 +3,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from geekshop.views import index, contacts
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +12,7 @@ urlpatterns = [
     path('contacts/', contacts, name="contacts"),
     path('products/', include('mainapp.urls', namespace="products"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
