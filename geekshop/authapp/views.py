f4abd6c8f1b9788085cf78906dd32979c5a22c2d
Yakov Chibizov <chibizov96@gmail.com>
2021-08-16 19:39:40 +0300

    Lesson 2.7 done

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,23 +1,24 @@
-from django.contrib import auth
 from django.core.mail import send_mail
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
 from django.urls import reverse
 
-from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
-from authapp.forms import ShopUserProfileEditForm
+from authapp.forms import ShopUserLoginForm, ShopUserEditForm, ShopUserRegisterForm, ShopUserProfileEditForm
 from authapp.models import ShopUser
+
 from geekshop import settings
-from django.db import transaction
 
 
 def send_verify_email(user):
     verify_link = reverse('auth:verify', args=[user.email, user.activation_key])
 
-    title = f'Активация пользователя {user.username}'
+    title = f'Активация на сайте, пользователя - {user.username}'
 
-    message = f'Для активации учетной записи {user.email} на сайте {settings.DOMAIN_NAME}' \
-              f'перейдите по ссылке \n{settings.DOMAIN_NAME}{verify_link}'
+    message = f'Для активации вашей учетной {user.email} записи, напортале {settings.DOMAIN_NAME}' \
+              f' перейдите по ссылке: \n{settings.DOMAIN_NAME}{verify_link}'
+
     return send_mail(title, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
 
 
@@ -38,21 +39,24 @@ def verify(request, email, activation_key):
 
 
 def login(request):
-    title = 'Вход в магазин'
+    title = 'вход'
+
     login_form = ShopUserLoginForm(data=request.POST)
+
     _next = request.GET['next'] if 'next' in request.GET.keys() else ''
-    #if request.POST and login_form.is_valid():
+
     if request.method == 'POST' and login_form.is_valid():
         username = request.POST['username']
         password = request.POST['password']
+
         user = auth.authenticate(username=username, password=password)
         if user and user.is_active:
             auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-            auth.login(request, user)
             if 'next' in request.POST.keys():
                 return HttpResponseRedirect(request.POST['next'])
             else:
                 return HttpResponseRedirect(reverse('index'))
+
     context = {
         'title': title,
         'login_form': login_form,
@@ -68,28 +72,32 @@ def logout(request):
 
 def register(request):
     title = 'регистрация'
+
     if request.method == 'POST':
         register_form = ShopUserRegisterForm(request.POST, request.FILES)
+
         if register_form.is_valid():
             user = register_form.save()
             if send_verify_email(user):
-                print('Письмо отправлено')
+                print('Сообщение отправлено.')
                 return HttpResponseRedirect(reverse('auth:login'))
             else:
-                print('Письмо не отправлено')
+                print('Неудлаось отправить сообщение.')
                 return HttpResponseRedirect(reverse('auth:login'))
     else:
         register_form = ShopUserRegisterForm()
+
     context = {
         'title': title,
         'register_form': register_form
     }
+
     return render(request, 'authapp/register.html', context)
 
 
 @transaction.atomic
 def edit(request):
-    title = 'СЂРµРґР°РєС‚РёСЂРѕРІР°РЅРёРµ'
+    title = 'редактирование'
 
     if request.method == 'POST':
         edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -19,18 +19,22 @@ from .views import index, contacts
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin_stuff/', include('adminapp.urls', namespace='admin_stuff')),
-    path('', index, name='index'),
-    path('contacts/', contacts, name='contacts'),
+    path('admin_staff/', include('adminapp.urls', namespace='admin_staff')),
+    path('', index, name="index"),
+    path('contacts/', contacts, name="contacts"),
     path('products/', include('mainapp.urls', namespace='products')),
-    path('auth/', include('authapp.urls', namespace='auth')),
     path('basket/', include('basketapp.urls', namespace='basket')),
     path('order/', include('ordersapp.urls', namespace='order')),
+    path('auth/', include('authapp.urls', namespace='auth')),
+
     path('', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
 
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
